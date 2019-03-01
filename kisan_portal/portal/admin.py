cb4936da88b82c25cbe203c544c98d073fab44f2
rahul <rahulmoorthy9.6@gmail.com>
2019-03-01 16:18:23 +0530

    models.py created and basic groundwork laid

---
+++ kisan_portal/portal/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import farmer_details,rent_details
+# Register your models here.
+
+admin.site.register(farmer_details)
+admin.site.register(rent_details)
