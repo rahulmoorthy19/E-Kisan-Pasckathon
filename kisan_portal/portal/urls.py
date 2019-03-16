c5a0633f8689b2f79b38ac040101eb4750d172a0
rahul <rahulmoorthy9.6@gmail.com>
2019-03-16 13:17:08 +0530

    yojna page and session control integrated

--- kisan_portal/portal/urls.py
+++ kisan_portal/portal/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.login, name='login'),
+	path('', views.login, name = 'login'),
     path('register', views.register, name='register'),
     path('about_us',views.about_us,name='about_us'),
     path('add_equipments',views.add_equipments,name='add_equipments'),
     path('profile',views.profile,name='profile'),
     path('plant_predict',views.plant_predict,name='plant_predict'),
-    path('water_predict',views.water_predict,name='water_predict')
+    path('water_predict',views.water_predict,name='water_predict'),
+    path('logout',views.logout,name='logout'),
+    path('yojna',views.yojna,name='yojna')
 ]
--- kisan_portal/portal/views.py
+++ kisan_portal/portal/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 import pandas as pd
 from .plot_yield import crop_yield
 from .plot_water import crop_water
+from django.urls import reverse
 # Create your views here.
 logged_in_user=farmer_user()
 def register(request):
@@ -22,62 +23,99 @@ def register(request):
 		if request.POST.get("pass")==request.POST.get("pass-confirm"):
 			farmer.password=request.POST.get("pass")
 			farmer.save()
-			return HttpResponseRedirect('')
+			return HttpResponseRedirect('login')
 		else:
 			messages.error(request,'password and confirm password not same!!!')
-
 	else:
 		return render(request,'portal/signup.html')
 
 
 def login(request):
-	if request.method=='POST':
-		farmer_reg_id=request.POST.get("username")
-		password=request.POST.get("pass")
-		authentication=farmer_user.objects.filter(farmer_reg_id=farmer_reg_id)
-		for users in authentication:
-			if(users.password==password):
-				logged_in_user.farmer_idno=users.farmer_idno
-				logged_in_user.farmer_reg_id=users.farmer_reg_id
-				logged_in_user.first_name=users.first_name
-				logged_in_user.last_name=users.last_name
-				logged_in_user.address=users.address
-				logged_in_user.district=users.district
-				logged_in_user.state=users.state
-				logged_in_user.age=users.age
-				logged_in_user.phone_no=users.phone_no
-				return HttpResponseRedirect('about_us')
-			else:
-				return render(request,'portal/login.html')
+	if request.session.has_key('username'):
+		username = request.session['username']
+		return HttpResponseRedirect('about_us')
 	else:
-		return render(request,'portal/login.html')
+		if request.method=='POST':
+			farmer_reg_id=request.POST.get("username")
+			password=request.POST.get("pass")
+			authentication=farmer_user.objects.filter(farmer_reg_id=farmer_reg_id)
+			for users in authentication:
+				if(users.password==password):
+					request.session['username'] = farmer_reg_id
+					logged_in_user.farmer_idno=users.farmer_idno
+					logged_in_user.farmer_reg_id=users.farmer_reg_id
+					logged_in_user.first_name=users.first_name
+					logged_in_user.last_name=users.last_name
+					logged_in_user.address=users.address
+					logged_in_user.district=users.district
+					logged_in_user.state=users.state
+					logged_in_user.age=users.age
+					logged_in_user.phone_no=users.phone_no
+					return HttpResponseRedirect('about_us')
+				else:
+					return render(request,'portal/login.html')
+		else:
+			return render(request,'portal/login.html')
 
 
 def about_us(request):
-	return render(request,'portal/dashboard.html')
+	if request.session.has_key('username'):
+		return render(request,'portal/dashboard.html')
+	else:
+		return HttpResponseRedirect(reverse('login'))
+
 
 def add_equipments(request):
-	if request.method=="POST":
-		equipment=rent_hire()
-		equipment.farmer_id_rent=farmer_user.objects.get(farmer_idno=logged_in_user.farmer_idno)
-		equipment.equipment_name=request.POST.get('equipname')
-		equipment.equipment_quantity=request.POST.get('quantity')
-		equipment.equipment_company=request.POST.get('company')
-		equipment.equipment_age=request.POST.get('usage')
-		equipment.equipment_renting_price=request.POST.get('price')
-		equipment.status_bit=1
-		equipment.save()
-	return render(request,'portal/addeq.html')
+	if request.session.has_key('username'):
+		if request.method=="POST":
+			equipment=rent_hire()
+			equipment.farmer_id_rent=farmer_user.objects.get(farmer_idno=logged_in_user.farmer_idno)
+			equipment.equipment_name=request.POST.get('equipname')
+			equipment.equipment_quantity=request.POST.get('quantity')
+			equipment.equipment_company=request.POST.get('company')
+			equipment.equipment_age=request.POST.get('usage')
+			equipment.equipment_renting_price=request.POST.get('price')
+			equipment.status_bit=1
+			equipment.save()
+		return render(request,'portal/addeq.html')
+	else:
+		return HttpResponseRedirect(reverse('login'))
+
+
 
 def profile(request):
-	return render(request,'portal/profile.html',{'user':logged_in_user})
+	if request.session.has_key('username'):
+		return render(request,'portal/profile.html',{'user':logged_in_user})
+	else:
+		return HttpResponseRedirect(reverse('login'))
 
 def plant_predict(request):
-	train=pd.read_csv('/home/sirzechlucifer/ML and ROS/e-Kisan/kisan_portal/portal/train.csv')
-	crop_yield(train,logged_in_user.district)
-	return render(request,'portal/plant_predict.html')
+	if request.session.has_key('username'):
+		train=pd.read_csv('/home/sirzechlucifer/ML and ROS/e-Kisan/kisan_portal/portal/train.csv')
+		crop_yield(train,logged_in_user.district)
+		return render(request,'portal/plant_predict.html')
+	else:
+		return HttpResponseRedirect(reverse('login'))
+
 
 def water_predict(request):
-	train=pd.read_csv('/home/sirzechlucifer/ML and ROS/e-Kisan/kisan_portal/portal/train.csv')
-	crop_water(train,logged_in_user.district)
-	return render(request,'portal/water_predict.html')
+	if request.session.has_key('username'):
+		train=pd.read_csv('/home/sirzechlucifer/ML and ROS/e-Kisan/kisan_portal/portal/train.csv')
+		crop_water(train,logged_in_user.district)
+		return render(request,'portal/water_predict.html')
+	else:
+		return HttpResponseRedirect(reverse('login'))
+
+def logout(request):
+	try:
+		del request.session['username']
+	except:
+		pass
+	return HttpResponseRedirect(reverse('login'))
+
+
+def yojna(request):
+	if request.session.has_key('username'):
+		return render(request,'portal/yojna.html')
+	else:
+		return HttpResponseRedirect(reverse('login'))
