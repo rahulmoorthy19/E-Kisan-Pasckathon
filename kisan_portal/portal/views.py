a86a0c77368d40cad9fad81415c30976e7750c27
rahul <rahulmoorthy9.6@gmail.com>
2019-03-16 13:59:18 +0530

    session variables handled and choice changes

--- kisan_portal/portal/views.py
+++ kisan_portal/portal/views.py
@@ -8,7 +8,6 @@ from .plot_yield import crop_yield
 from .plot_water import crop_water
 from django.urls import reverse
 # Create your views here.
-logged_in_user=farmer_user()
 def register(request):
 	if request.method=='POST':
 		farmer=farmer_user()
@@ -31,8 +30,7 @@ def register(request):
 
 
 def login(request):
-	if request.session.has_key('username'):
-		username = request.session['username']
+	if request.session.has_key('farmer_reg_id'):
 		return HttpResponseRedirect('about_us')
 	else:
 		if request.method=='POST':
@@ -41,16 +39,15 @@ def login(request):
 			authentication=farmer_user.objects.filter(farmer_reg_id=farmer_reg_id)
 			for users in authentication:
 				if(users.password==password):
-					request.session['username'] = farmer_reg_id
-					logged_in_user.farmer_idno=users.farmer_idno
-					logged_in_user.farmer_reg_id=users.farmer_reg_id
-					logged_in_user.first_name=users.first_name
-					logged_in_user.last_name=users.last_name
-					logged_in_user.address=users.address
-					logged_in_user.district=users.district
-					logged_in_user.state=users.state
-					logged_in_user.age=users.age
-					logged_in_user.phone_no=users.phone_no
+					request.session['farmer_idno']=users.farmer_idno
+					request.session['farmer_reg_id']=users.farmer_reg_id
+					request.session['first_name']=users.first_name
+					request.session['last_name']=users.last_name
+					request.session['address']=users.address
+					request.session['district']=users.district
+					request.session['state']=users.state
+					request.session['age']=users.age
+					request.session['phone_no']=users.phone_no
 					return HttpResponseRedirect('about_us')
 				else:
 					return render(request,'portal/login.html')
@@ -59,14 +56,14 @@ def login(request):
 
 
 def about_us(request):
-	if request.session.has_key('username'):
+	if request.session.has_key('farmer_reg_id'):
 		return render(request,'portal/dashboard.html')
 	else:
 		return HttpResponseRedirect(reverse('login'))
 
 
 def add_equipments(request):
-	if request.session.has_key('username'):
+	if request.session.has_key('farmer_reg_id'):
 		if request.method=="POST":
 			equipment=rent_hire()
 			equipment.farmer_id_rent=farmer_user.objects.get(farmer_idno=logged_in_user.farmer_idno)
@@ -84,13 +81,23 @@ def add_equipments(request):
 
 
 def profile(request):
-	if request.session.has_key('username'):
+	if request.session.has_key('farmer_reg_id'):
+		logged_in_user=farmer_user()
+		logged_in_user.farmer_idno= request.session['farmer_idno']
+		logged_in_user.farmer_reg_id=request.session['farmer_reg_id']
+		logged_in_user.first_name=request.session['first_name']
+		logged_in_user.last_name=request.session['last_name']
+		logged_in_user.address=request.session['address']
+		logged_in_user.district=request.session['district']
+		logged_in_user.state=request.session['state']
+		logged_in_user.age=request.session['age']
+		logged_in_user.phone_no=request.session['phone_no']
 		return render(request,'portal/profile.html',{'user':logged_in_user})
 	else:
 		return HttpResponseRedirect(reverse('login'))
 
 def plant_predict(request):
-	if request.session.has_key('username'):
+	if request.session.has_key('farmer_reg_id'):
 		train=pd.read_csv('/home/sirzechlucifer/ML and ROS/e-Kisan/kisan_portal/portal/train.csv')
 		crop_yield(train,logged_in_user.district)
 		return render(request,'portal/plant_predict.html')
@@ -99,7 +106,7 @@ def plant_predict(request):
 
 
 def water_predict(request):
-	if request.session.has_key('username'):
+	if request.session.has_key('farmer_reg_id'):
 		train=pd.read_csv('/home/sirzechlucifer/ML and ROS/e-Kisan/kisan_portal/portal/train.csv')
 		crop_water(train,logged_in_user.district)
 		return render(request,'portal/water_predict.html')
@@ -108,14 +115,22 @@ def water_predict(request):
 
 def logout(request):
 	try:
-		del request.session['username']
+		del request.session['farmer_reg_id']
+		del request.session['farmer_idno']
+		del request.session['first_name']
+		del request.session['last_name']
+		del request.session['address']
+		del request.session['district']
+		del request.session['state']
+		del request.session['age']
+		del request.session['phone_no']
 	except:
 		pass
 	return HttpResponseRedirect(reverse('login'))
 
 
 def yojna(request):
-	if request.session.has_key('username'):
+	if request.session.has_key('farmer_reg_id'):
 		return render(request,'portal/yojna.html')
 	else:
 		return HttpResponseRedirect(reverse('login'))
